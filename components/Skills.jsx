3d08d6bb142d0b552864e3eeca2498d9b377aa73
TrymoretimeH <pvhcnptit@gmail.com>
2023-07-09 18:42:52 +0700

    update image path to correct nextjs syntax

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -1,5 +1,13 @@
 import Image from "next/image";
 import React from "react";
+import htmlImg from "../public/assets/skills/html.png"
+import cssImg from "../public/assets/skills/css.png"
+import javascriptImg from "../public/assets/skills/javascript.png"
+import reactImg from "../public/assets/skills/react.png"
+import nodeImg from "../public/assets/skills/node.png"
+import nextImg from "../public/assets/skills/nextjs.png"
+import tailwindImg from "../public/assets/skills/tailwind.png"
+import githubImg from "../public/assets/skills/github1.png"
 
 const Skills = () => {
   return (
@@ -15,7 +23,7 @@ const Skills = () => {
             <div className="grid grid-cols-2 gap-4 justify-center items-center">
               <div className="m-auto">
                 <Image
-                  src="/../public/assets/skills/html.png"
+                  src={htmlImg}
                   alt="/"
                   width="64"
                   height="64"
@@ -31,7 +39,7 @@ const Skills = () => {
             <div className="grid grid-cols-2 gap-4 justify-center items-center">
               <div className="m-auto">
                 <Image
-                  src="/../public/assets/skills/css.png"
+                  src={cssImg}
                   alt="/"
                   width="64"
                   height="64"
@@ -47,7 +55,7 @@ const Skills = () => {
             <div className="grid grid-cols-2 gap-4 justify-center items-center">
               <div className="m-auto">
                 <Image
-                  src="/../public/assets/skills/javascript.png"
+                  src={javascriptImg}
                   alt="/"
                   width="64"
                   height="64"
@@ -63,7 +71,7 @@ const Skills = () => {
             <div className="grid grid-cols-2 gap-4 justify-center items-center">
               <div className="m-auto">
                 <Image
-                  src="/../public/assets/skills/react.png"
+                  src={reactImg}
                   alt="/"
                   width="64"
                   height="64"
@@ -79,7 +87,7 @@ const Skills = () => {
             <div className="grid grid-cols-2 gap-4 justify-center items-center">
               <div className="m-auto">
                 <Image
-                  src="/../public/assets/skills/node.png"
+                  src={nodeImg}
                   alt="/"
                   width="64"
                   height="64"
@@ -95,7 +103,7 @@ const Skills = () => {
             <div className="grid grid-cols-2 gap-4 justify-center items-center">
               <div className="m-auto">
                 <Image
-                  src="/../public/assets/skills/tailwind.png"
+                  src={tailwindImg}
                   alt="/"
                   width="64"
                   height="64"
@@ -111,7 +119,7 @@ const Skills = () => {
             <div className="grid grid-cols-2 gap-4 justify-center items-center">
               <div className="m-auto">
                 <Image
-                  src="/../public/assets/skills/nextjs.png"
+                  src={nextImg}
                   alt="/"
                   width="64"
                   height="64"
@@ -127,7 +135,7 @@ const Skills = () => {
             <div className="grid grid-cols-2 gap-4 justify-center items-center">
               <div className="m-auto">
                 <Image
-                  src="/../public/assets/skills/github1.png"
+                  src={githubImg}
                   alt="/"
                   width="64"
                   height="64"
